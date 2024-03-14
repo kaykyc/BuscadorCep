235b6ddabce0c98c2ad52abfe45ada5454c17ec2
kaykyc <akc.silva@discente.ufma.br>
2024-03-14 11:27:39 -0300

    Initial commit

---
+++ src/BuscadorDeCep.java
@@ -0,0 +1,9 @@
+public class BucadorDeCep {
+    public static void main(String[] args) {
+
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+        System.out.println(novoEndereco);
+
+    }
+}
